039eaa89a7b0e6d1128a857dcd69b0d3d1c3aad5
Young Foo <mr.f.young@gmail.com>
2015-09-04 18:34:22 +0800

    commit browser js

--- js/ton.core.browser.js
+++ js/ton.core.browser.js
@@ -1,4 +1,10 @@
-﻿TON.UA = {
+﻿/**
+ * @description 特征检测
+ * @todo 以下方法计划将全部改写。从UA迁移至特征检测
+ * @contacts: Young, Cherishope
+ */
+
+TON.UA = {
     isTouchDevice:function(){
         return !!('ontouchstart' in window) // works on most browsers 
             || !!('onmsgesturechange' in window); // works on ie10 
