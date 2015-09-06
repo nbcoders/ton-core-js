8845555baac7eec1694e080379aa306fce481875
Young Foo <mr.f.young@gmail.com>
2015-09-06 12:39:50 +0800

    Recode Ton.Device function
    Recode Ton.Device function for detect the device

--- js/ton.core.browser.js
+++ js/ton.core.browser.js
@@ -4,28 +4,60 @@
  * @contacts: Young, Cherishope
  */
 
-TON.UA = {
+TON.Browser = {
+
+}
+
+TON.Device = {
+
+    //if touch device
     isTouchDevice:function(){
-        return !!('ontouchstart' in window) // works on most browsers 
-            || !!('onmsgesturechange' in window); // works on ie10 
+        return ('ontouchstart' in window) // works on most browsers
+            || ('onmsgesturechange' in window); // works on ie10
     },
+
+    //if IE mobile device
     isIEMobile:function(){
-        return !!(window.navigator.msPointerEnabled);
+        return !!(navigator.msPointerEnabled);
     },
+
+    //if mobile device, this function is recomend by MDN
+    //if this mobile device means mobile phones, except tablets, like this
+
+    //Phone UA:
+    //Mozilla/5.0 (Linux; <Android Version>; <Build Tag etc.>) AppleWebKit/<WebKit Rev> (KHTML, like Gecko) Chrome/<Chrome Rev> Mobile Safari/<WebKit Rev>
+
+    //Tablet UA:
+    //Mozilla/5.0 (Linux; <Android Version>; <Build Tag etc.>) AppleWebKit/<WebKit Rev>(KHTML, like Gecko) Chrome/<Chrome Rev> Safari/<WebKit Rev>
+
+    isMobile: function () {
+        return navigator.userAgent.match(/Mobi/i);
+    },
+
+    //this function is only used for Android Tablet
+    isTablet: function (){
+        return navigator.userAgent.match(/Tablet/i);
+    },
+
+    //apple device
     isAppleDevice:function(){
-        return (navigator.platform.indexOf('iPad') != -1);
+        return this.isIPHONE() || this.isIPOD() || this.isIPAD();
     },
-    //is iPod or iPhone
+    
+    //navigator.platform iPhone device return "iPhone", iPad device return "iPad"
+    //navigator.platform Linux and Android device will return "Linux aarch64"
+    //navigator.platform Windows will return "Win32"
     isIPHONE:function(){
-        return navigator.userAgent.match(/iPhone/i);
+        return navigator.platform.match(/iPhone/i);
     },
     isIPOD:function(){
-        return navigator.userAgent.match(/iPod/i);
+        return navigator.platform.match(/iPod/i);
     },
     isIPAD:function(){
-        return navigator.userAgent.match(/iPod/i);
+        return navigator.platform.match(/iPad/i);
     },
-    //is iPod or iPhone
+
+    //if it is iPod or iPhone
     isAppleMobile:function(){
         return navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i);
     }
