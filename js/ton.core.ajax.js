2688b4b60f87f606c59c856f92d40f10d8f0a77e
by701 <by701@nbcoders.com>
2015-09-10 00:00:23 +0800

    add ton.core.date.js
    add some func to formate the date

--- js/ton.core.ajax.js
+++ js/ton.core.ajax.js
@@ -1,9 +1,16 @@
-/**
-* 提供Ajax相关操作
-**/
-(function (ton){
-	var ajax = {};
-	//TODO 
-	ton.Ajax = ajax;
-	window.Ton= ton;
+/***
+ * some easy method for ajax
+ */
+(function(ton) {
+	function A(url, type, data, callback) {
+		console.log("ajax");
+	}
+	A.post = function(url, data, callback) {
+		console.log("post");
+	}
+	A.get = function(url, data, callback) {
+		console.log("get");
+	}
+	ton.Ajax = A;
+	window.Ton = ton;
 })(typeof Ton !== "undefined" ? Ton : {});
---
+++ js/ton.core.date.js
@@ -0,0 +1,47 @@
+/***
+ * some method for format date
+ */
+(function(ton) {
+	var D = {};
+	//TODO should suport 24 and 12 
+	/**
+	 * format the date
+	 * @param {Object} date the date you will format
+	 * @param {Object} [str] the string of format the date eg: YYYY-MM-DD hh:mm:ss
+	 */
+	D.format = function(date, str) {
+		if (arguments.length === 0) {
+			console.log("parameter is not correct!");
+			return;
+		}
+		str = str || "YYYY-MM-DD hh:mm:ss";
+		var _date = new Date(date);
+		var _year = _date.getFullYear();
+		var _month = _date.getMonth() + 1;
+		var _day = _date.getDate();
+		var _hours = _date.getHours();
+		var _minutes = _date.getMinutes();
+		var _seconds = _date.getSeconds();
+		_month = _month < 10 ? "0" + _month : _month;
+		_day = _month < 10 ? "0" + _day : _day;
+		_hours = _hours < 10 ? "0" + _hours : _hours;
+		_minutes = _minutes < 10 ? "0" + _minutes : _minutes;
+		_seconds = _seconds < 10 ? "0" + _seconds : _seconds;
+		str = str.replace(/YYYY/, _year);
+		str = str.replace(/MM/, _month);
+		str = str.replace(/DD/, _day);
+		str = str.replace(/hh/, _hours);
+		str = str.replace(/mm/, _minutes);
+		str = str.replace(/ss/, _seconds);
+		return str;
+	};
+	/**
+	 *
+	 * @param {Object} str
+	 */
+	D.getDate = function(str) {
+		return D.format(new Date());
+	};
+	ton.Date = D;
+	window.Ton = ton;
+})(typeof Ton !== "undefined" ? Ton : {});
