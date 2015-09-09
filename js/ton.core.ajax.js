67e50c5a9e5cc33e4236590c1c3db1a106104aa8
by701 <by701@nbcoders.com>
2015-09-09 16:09:15 +0800

    约定组件封装规则
    增加ton.core.js的描述已经统一底层组件封装规则

---
+++ js/ton.core.ajax.js
@@ -0,0 +1,9 @@
+/**
+* 提供Ajax相关操作
+**/
+(function (ton){
+	var ajax = {};
+	//TODO 
+	ton.Ajax = ajax;
+	window.Ton= ton;
+})(typeof Ton !== "undefined" ? Ton : {});
---
+++ ton.core.js
@@ -0,0 +1,5 @@
+﻿/***
+* 此文件是由用户动态定制生成
+* TODO some info about ton-core-js
+***/
+
