277e51cc658d2c218c2c11049f5b27997e404e6d
by701 <by701@nbcoders.com>
2015-09-10 21:32:11 +0800

    md5.js date.js test.html
    done md5.js and modify the date.js create a test.html to test the code

--- js/ton.core.ajax.js
+++ js/ton.core.ajax.js
@@ -2,15 +2,15 @@
  * some easy method for ajax
  */
 (function(ton) {
-	function A(url, type, data, callback) {
-		console.log("ajax");
-	}
-	A.post = function(url, data, callback) {
-		console.log("post");
-	}
-	A.get = function(url, data, callback) {
-		console.log("get");
-	}
-	ton.Ajax = A;
-	window.Ton = ton;
+    function A(url, type, data, callback) {
+        console.log("ajax");
+    }
+    A.post = function(url, data, callback) {
+        console.log("post");
+    }
+    A.get = function(url, data, callback) {
+        console.log("get");
+    }
+    ton.Ajax = A;
+    window.Ton = ton;
 })(typeof Ton !== "undefined" ? Ton : {});
